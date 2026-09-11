47f1b04c10b2b1309b9ee0bf1a1fd1aa3a00f2c3
Gabriela Uribe <gabrielauribe484@gmail.com>
2026-09-11 08:07:09 -0500

    Se realizó el README.md

---
+++ src/Libro.java
@@ -0,0 +1,18 @@
+public class Libro {
+    private String titulo;
+    private String autor;
+    private String codigo;
+    private String genero;
+    private int anioPublicacion;
+    private int copiasDisponibles;
+
+    public Libro (String titulo, String autor, String codugo, String genero, int anioPublicacion, int copiasDisponibles) {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.codigo = codigo;
+        this.genero = genero;
+        this.anioPublicacion = anioPublicacion;
+        this.copiasDisponibles = copiasDisponibles;
+
+    }
+}
