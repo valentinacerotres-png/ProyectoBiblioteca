f95e2925afe3863b3e2ca97fce1f25ce97ccde9c
Gabriela Uribe <gabrielauribe484@gmail.com>
2026-09-16 09:28:35 -0500

    Se empezó a realizar el ejercicio de la ventana principal

---
+++ src/VentanaPrincipal.java
@@ -0,0 +1,9 @@
+import javax.swing.*;
+
+public class VentanaPrincipal extends JFrame {
+    private Biblioteca biblioteca;
+    private JTextField txtTitulo;
+    private JTextField txtAutor;
+
+
+}
